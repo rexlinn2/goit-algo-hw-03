afb142db7ce66e38812c7909f01e5538e08bc6a6
Ruslan Hrymalovskyi <rgrimalovskiy@gmail.com>
2026-04-22 22:28:05 +0300

    none

--- task_1.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-def get_days_from_today(date):
-    given_date = datetime.strptime(date, "%Y-%m-%d").date()
-    today = datetime.today().date()
-    return (today - given_date).days
-
-print(get_days_from_today("2026-04-21"))
